7491efd2cb7fe87a23aed4918dbfae1cfb2af875
Tesia Tran <tesiatran88@gmail.com>
2021-02-24 12:43:21 -0800

    Defined 'logged_in?' method to check if user is logged in or not -- returns boolean.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::API
   before_action :require_login
 
+  def logged_in?
+    !!user_logged_in
+  end
+
   def require_login
     render json: { message: 'Please log in to access this feature.' }, status: :unauthorized unless logged_in?
   end
