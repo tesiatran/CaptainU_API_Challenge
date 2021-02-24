d074970b2c569f708184c0c0be591216c10890b9
Tesia Tran <tesiatran88@gmail.com>
2021-02-24 12:28:05 -0800

    Added 'before' method to call 'require_login' method first when requests are made. The 'require_login' method returns JSON response asking user to log in if unauthorized.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  before_action :require_login
+
+  def require_login
+    render json: { message: 'Please log in to access this feature.' }, status: :unauthorized unless logged_in?
+  end
 end
