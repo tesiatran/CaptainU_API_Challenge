22099f0afa32b565f0a77389c27e5366b2389310
Tesia Tran <tesiatran88@gmail.com>
2021-02-24 20:09:42 -0800

    Generated tournaments controller file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'tournaments/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:create]
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
-  get "/tournaments", to: "tournament#index"
+  get "/tournaments", to: "tournaments#index"
 end
---
+++ test/controllers/tournaments_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class TournamentsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get tournaments_index_url
+    assert_response :success
+  end
+end
