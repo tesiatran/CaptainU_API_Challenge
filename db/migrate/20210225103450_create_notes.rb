45445034bcc3f529af3177029b643226d13562b3
Tesia Tran <tesiatran88@gmail.com>
2021-02-25 02:35:56 -0800

    Generated model for Note according to specifications.

---
+++ db/migrate/20210225103450_create_notes.rb
@@ -0,0 +1,13 @@
+class CreateNotes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :notes do |t|
+      t.integer :user_id
+      t.string :note
+      t.integer :assessment_id
+      t.string :created_at
+      t.string :updated_at
+
+      t.timestamps
+    end
+  end
+end
